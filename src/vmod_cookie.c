ecb71c9e7fa69178206a892a125ea201aa3d73b8
Lasse Karstensen <lasse@varnish-software.com>
2012-04-03 13:33:36 +0200

    s/example/cookie/g

--- src/vmod_cookie.c
+++ src/vmod_cookie.c
@@ -12,21 +12,37 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 }
 
 const char *
-vmod_hello(struct sess *sp, const char *name)
+vmod_parse(struct sess *sp, const char *cookieheader)
+{
+	char *p;
+	unsigned u, v;
+	// TODO: run through cookieheader string, split on ; and 
+	// make an assosiative array out of it.
+	return (p);
+}
+
+const char *
+vmod_get_string(struct sess *sp, const char *cookiename)
 {
 	char *p;
 	unsigned u, v;
 
-	u = WS_Reserve(sp->wrk->ws, 0); /* Reserve some work space */
-	p = sp->wrk->ws->f;		/* Front of workspace area */
-	v = snprintf(p, u, "Hello, %s", name);
+/*	u = WS_Reserve(sp->wrk->ws, 0); /* Reserve some work space */
+	//p = sp->wrk->ws->f;		/* Front of workspace area */
+
+/* #	v = sprintf(name, "Hello foo"); // snprintf(p, u, "Hello, %s", name);
 	v++;
 	if (v > u) {
-		/* No space, reset and leave */
 		WS_Release(sp->wrk->ws, 0);
 		return (NULL);
 	}
-	/* Update work space with what we've used */
 	WS_Release(sp->wrk->ws, v);
+	*/
 	return (p);
 }
+
+#ifdef STANDALONE
+void main(int argc, char* argv[]) {
+    printf("ohai\n");
+}
+#endif 
