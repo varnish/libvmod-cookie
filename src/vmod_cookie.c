c242e95097f3ea91244d8ca487513fb16e09af54
Lasse Karstensen <lasse@varnish-software.com>
2012-07-11 20:27:32 +0200

    Yes, that v variable is the number of octets in the string ..

--- src/vmod_cookie.c
+++ src/vmod_cookie.c
@@ -84,40 +84,49 @@ int hash_cleanup() {
 }
 
 int parse(char *cookieheader) { 
-    char *token, *tokendata, *subtokendata, *key, *value, *sepindex;
-    char *tokstate;
-    char cookies[4096];
-    char tokenitem[4096];
+    char tokendata[MAX_COOKIESTRING];
+    char *token, *tokstate, *key, *value, *sepindex;
+    char *dataptr = tokendata;
 
     struct cookie *new;
 
     if (cookieheader == NULL) return(255);
     else if (strlen(cookieheader) == 0) return(255);
+    else if (strlen(cookieheader) >= MAX_COOKIESTRING) return(255);
 
-//    printf("Input is: \"\"\"%s\"\"\"\n", cookieheader);
-    tokendata = cookies;
     strcpy(tokendata, cookieheader); // strtok modifies source, fewer surprises.
+    dataptr = tokendata;
 
     while (1) {
-	token = strtok_r(tokendata, ";", &tokstate);
-	tokendata = NULL; // strtok() wants NULL on subsequent calls.
+	token = strtok_r(dataptr, ";", &tokstate);
+	dataptr = NULL; // strtok() wants NULL on subsequent calls.
+
 	if (token == NULL) break;
-	if (token[0] == ' ') token++;
+        while (token[0] == ' ') token++;
+        //printf("token is: %s\n", token);
 
-	strcpy(tokenitem, token);
-	sepindex = strchr(tokenitem, '='); 
+	sepindex = strchr(token, '='); 
+        if (sepindex == NULL) { 
+            // could not find delimiter, this cookie is invalid. skip to the next (if any)
+            continue; 
+        }
 	value = sepindex + 1;
-
 	*sepindex = '\0';
-	key = tokenitem;
+
+        if (strlen(key)   > MAX_COOKIEPART) continue;
+        if (strlen(value) > MAX_COOKIEPART) continue;
+
+        //printf("token is: %s\n", token);
+        //printf("value is: %s\n", value);
 
         new = malloc(sizeof(struct cookie));
         if (new == NULL) return(255);
-        strcpy(new->name, key);
+        strcpy(new->name, token);
         strcpy(new->value, value);
         hash_set(new);
+
 #if STANDALONE
-	printf("parsed token; key:%s value:%s\n", key, value);
+//	printf("parsed token; key:%s value:%s\n", key, value);
 #endif
     }
     return(0);
@@ -127,19 +136,24 @@ int get_string(char *cookiestring) {
     struct cookie *s;
     char *stringpointer = cookiestring;
     char cookieitem[MAX_COOKIESTRING];
-    int curlen = 0;
+    size_t curlen = 0;
+
     for (s=cookies; s != NULL; s=s->hh.next) {
-        sprintf(cookieitem, "%s:%s; ", s->name, s->value);
+        sprintf(cookieitem, "%s=%s", s->name, s->value);
+        //sprintf(cookieitem, "%s", s->name);
+        if (s->hh.next != NULL) strcat(cookieitem, "; ");
+
         // XXX: verify error handling
-        curlen += (int)strlen(cookieitem);
+        curlen += strlen(cookieitem);
         if (curlen >= MAX_COOKIESTRING) {
             cookiestring = '\0'; // zero out so that we're sure to notice the error ;)
-            return(255);
+ //           *stringpointer = '\0';
+            return(0);
         }
         strcat(stringpointer, cookieitem);
         memset(cookieitem, '\0', sizeof(cookieitem));
     }
-    return(0);
+    return(curlen);
 }
 
 int init_function(struct vmod_priv *priv, const struct VCL_conf *conf) {
@@ -168,19 +182,25 @@ void vmod_clean(struct sess *sp) {
     hash_cleanup();
 }
 
+// no WS_Reserve, must fix makefile..
+#ifndef STANDALONE 
 const char * vmod_get_string(struct sess *sp) {
     char *p;
     unsigned u, v;
 
-    char cookiestring[MAX_COOKIESTRING];
-//    cookiestring = malloc(MAX_COOKIESTRING);
-    get_string((char *)&cookiestring);
+    char *cookiestring;
+    cookiestring = malloc(MAX_COOKIESTRING);
+    if (cookiestring == NULL) return(NULL);
+
+    get_string(cookiestring);
 
     u = WS_Reserve(sp->wrk->ws, 0);
     p = sp->wrk->ws->f;	
 
     v = snprintf(p, u, "%s", cookiestring);
- //   free(cookiestring);
+//    v = sprintf(p, cookiestring);
+//    printf("v is %i\n", v);
+    free(cookiestring);
     v++;
     if (v > u) {
             WS_Release(sp->wrk->ws, 0);
@@ -189,14 +209,18 @@ const char * vmod_get_string(struct sess *sp) {
     WS_Release(sp->wrk->ws, v);
     return (p);
 }
+#endif
 
 #ifdef STANDALONE
 void test_parse() {
 //    char* input = "cookie1=parsed1;cookie2=parsed2;cookie3=parsed3;";
-    char *input = "__utma=21840418.36054577.1327330061.1333353182.1333370469.20; __utmb=21840418.490.10.1333370469; __utmc=21840418; __utmz=21840418.1327330061.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=foo";
+//    char *input = "__utma=21840418.36054577.1327330061.1333353182.1333370469.20; __utmb=21840418.490.10.1333370469; __utmc=21840418; __utmz=21840418.1327330061.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=foo";
+//    char *input = "csrftoken=0e0c3616e41a6bd561b72b7f5fc1128f; sessionid=a707505310ddf259bb290d3ca63fc560";
+    char *input = "csrftoken=0e0c3616e41a6bd561b72b7f5fc1128f;sessionid=a707505310ddf259bb290d3ca63fc560\0";
     char empty[] = "";
     parse(NULL);
     parse(empty);
+    printf("Input is: \"\"\"%s\"\"\"\n", input);
     parse(input);
     char *cookiestring;
     cookiestring = malloc(MAX_COOKIESTRING);
@@ -205,7 +229,6 @@ void test_parse() {
 }
 
 void test_simple() {
-
     struct cookie cookie1 = { .name = "cookie1", .value = "setvalue1" };
     struct cookie cookie2 = { .name = "cookie2", .value = "setvalue2" };
     struct cookie *s = NULL;
